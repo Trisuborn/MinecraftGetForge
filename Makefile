de177675e29472c02f9f47471afd9b745dd6122d
Trisuborn <553070703@qq.com>
2021-02-15 15:31:34 +0800

    2

--- Makefile
+++ Makefile
@@ -1,8 +1,26 @@
 # tools
-PYUIC 	= pyuic5.exe
+PYUIC 		= pyuic5.exe
+PYINSTALL	= pyinstaller.exe
+
+RM 			= rm
+MV 			= mv
+
+# Flags
+PYINSTALL_FLAGS = -Fw
+
+# FILES
+PYINSTALL_FILES  = main.py
+PYINSTALL_FILES += ./mgf_qt_ui/ui_py/$(MGF_MAIN).py
 
 # MGF_MAIN
 MGF_MAIN = mgf_main
 
-all:
+uic:
 	$(PYUIC) -o ./mgf_qt_ui/ui_py/$(MGF_MAIN).py ./mgf_qt_ui/ui_xml/$(MGF_MAIN).ui
+
+pack:
+	$(PYINSTALL) $(PYINSTALL_FLAGS) $(PYINSTALL_FILES)
+
+clean:
+	rm -rf ./dist ./build ./main.spec ./__pycache__
+
--- main.py
+++ main.py
@@ -5,10 +5,32 @@ from PyQt5.QtWidgets import QMainWindow
 # mgf ui
 import mgf_qt_ui.ui_py.mgf_main as mgf_ui_main
 
+def mgf_init(main_window):
+    mgf_ui = mgf_ui_main.Ui_MainWindow()
+    mgf_ui.setupUi(main_window)
+
+    mgf_ui.textBrowser.clear()
+    mgf_ui.textBrowser.append("<a href=\"https://github.com/Trisuborn/MinecraftGetForge\">项目</a>")
+    mgf_ui.textBrowser.append("<a href=\"https://github.com/Trisuborn/MinecraftGetForge\">项目</a>")
+    mgf_ui.textBrowser.append("<a href=\"https://github.com/Trisuborn/MinecraftGetForge\">项目</a>")
+    mgf_ui.textBrowser.append("<a href=\"https://github.com/Trisuborn/MinecraftGetForge\">项目</a>")
+    mgf_ui.textBrowser.append("<a href=\"https://github.com/Trisuborn/MinecraftGetForge\">项目</a>")
+    mgf_ui.textBrowser.append("<a href=\"https://github.com/Trisuborn/MinecraftGetForge\">项目</a>")
+    mgf_ui.textBrowser.append("<a href=\"https://github.com/Trisuborn/MinecraftGetForge\">项目</a>")
+    mgf_ui.textBrowser.append("<a href=\"https://github.com/Trisuborn/MinecraftGetForge\">项目</a>")
+    mgf_ui.textBrowser.append("<a href=\"https://github.com/Trisuborn/MinecraftGetForge\">项目</a>")
+    mgf_ui.textBrowser.append("<a href=\"https://github.com/Trisuborn/MinecraftGetForge\">项目</a>")
+    mgf_ui.textBrowser.append("<a href=\"https://github.com/Trisuborn/MinecraftGetForge\">项目</a>")
+    mgf_ui.textBrowser.append("<a href=\"https://github.com/Trisuborn/MinecraftGetForge\">项目</a>")
+    mgf_ui.textBrowser.append("<a href=\"https://github.com/Trisuborn/MinecraftGetForge\">项目</a>")
+
+
+# main
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     MainWindow = QMainWindow()
-    ui = mgf_ui_main.Ui_MainWindow()
-    ui.setupUi(MainWindow)
+
+    mgf_init(MainWindow)
+
     MainWindow.show()
     sys.exit(app.exec_())
